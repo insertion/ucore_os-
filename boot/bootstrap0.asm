fda994080ec51992f65017692e769285f7f02a8b
wanglei <398758696@qq.com>
2024-08-11 20:11:28 +0800

    fix code

--- boot/bootstrap0.asm
+++ boot/bootstrap0.asm
@@ -1,19 +1,19 @@
 	;; bootstrap.asm
-	;; CPU start in real mode,and BIOS load code at address 0000��7c00
-	org 7c00h		;ָʾ���򽫱����ص�7c00h������������ı��ڴ�Ķ���ƫ��
-	xor ax,ax		;��ax�Ĵ�����0
+	;; CPU start in real mode,and BIOS load code at address 0000：7c00
+	org 7c00h		;指示程序将被加载到7c00h处，编译器会改变内存的段内偏移
+	xor ax,ax		;将ax寄存器清0
 	mov ds,ax		;filled DS with 0
 
-	mov si,msg		;���ַ����ĵ�ַд��si��ַ�Ĵ���
-	cld			;����df��־�Ĵ���
+	mov si,msg		;将字符串的地址写入si变址寄存器
+	cld			;设置df标志寄存器
 	
 ch_loop:
-	lodsb			;��DS:SI����byteд��AL
-	test al,al		;al�ﱣ�����ַ���msg���һ����0
+	lodsb			;将DS:SI处的byte写入AL
+	test al,al		;al里保存着字符，msg最后一个是0
 	jz loop
-	mov ah,0x0e		;0x0e ���ܺź�0x13���ܺţ��ԼĴ���ʹ�÷�ʽ��һ��
+	mov ah,0x0e		;0x0e 功能号和0x13功能号，对寄存器使用方式不一样
 	mov bh,0		;
-	int 0x10		;bios�ṩ���жϷ��񣬹��ܺ�13λ��ʾ����
+	int 0x10		;bios提供的中断服务，功能号13位显示服务
 	jmp ch_loop
 loop:
 	jmp loop		;dead loop
